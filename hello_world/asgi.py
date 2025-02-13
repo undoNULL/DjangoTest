c89e7d4ede5e18803084dac2f726386ab65006c4
Graha-Tia <wkdbdls9977@naver.com>
2025-02-14 04:02:27 +0900

    test commit

--- hello_world/asgi.py
+++ hello_world/asgi.py
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_world.settings')
 
 application = get_asgi_application()
+
+adsadasfagsggsd
